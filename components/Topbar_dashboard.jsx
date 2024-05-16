96340dbbebc294935236ecd0395cc64cce1c8819
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-05-17 06:24:53 +0700

    test: Tes commit Del Topbar_dashboard

--- components/Topbar_dashboard.jsx
+++
@@ -1,7 +0,0 @@
-export default function Topbar_dashboard(){
-    return (
-        <div className="flex h-auto w-full items-center justify-center">
-            <h1 className="text-4xl font-bold">Topbar Page</h1>
-        </div>
-    );
-}
