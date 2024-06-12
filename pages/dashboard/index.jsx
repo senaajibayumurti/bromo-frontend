1b61f4b36b65808413fed0e53663b81534b0f70b
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-06-13 00:06:23 +0700

    fix: minor edits

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -6,27 +6,11 @@ import RoundedContainer from "../../components/Layout/RoundedContainer";
 import DonutChart from "../../components/Charts/DonutChart";
 
 const Beranda = () => {
-    const [forecastingData, setForecastingData] = useState(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://api.example.com/forecasting-data');
-                const data = await response.json();
-                setForecastingData(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
     return (
         <DashboardLayout>
             <DropdownKandang s/>
             <RoundedContainer label={'Forecasting'}>
-                <DonutChart data={forecastingData} />
+                {/* <DonutChart /> */}
             </RoundedContainer>
             <RoundedContainer label={'Klasifikasi'}>
                 <FormKlasifikasi/>
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,37 +2,42 @@ import Topbar from '../components/TopBar/Topbar_landingpage';
 import Circle from '../components/Temp_circle';
 import Icon from '../components/Icon_arrow-right-end-on-rectangle';
 import Button from '../components/Button';
-
 export default function Home() {
   return (
-    <div className="h-screen w-auto ">
-        <div className="flex flex-col w-3/4 h-screen mx-auto">
-            <div className="h-min w-full my-4">
-                <Topbar/>
-            </div>
-            <div className="flex w-full h-screen items-center justify-center">
-              <div class="grid grid-cols-2 gap-4">
-                <div className="flex flex-col justify-center">
-                  <article class="text-wrap">
-                    <h1 className="text-4xl font-bold mb-4 text-green-600">Solusi Terbaik untuk Memantau Kandang Ayam Anda!</h1>
-                    <p className="text-bromo-gray-500">Broiler Monitoring, aplikasi yang dirancang untuk meningkatkan efektifitas pemantauan kandang ayam secara efektif dan efisen secara real-time.</p>
-                  </article>
-                  <div>
-                    <Button label="Tombol"/>
-                  </div>
-                  <div className='mt-8'>
-                    <button type="button" class="inline-flex items-center text-bromo-gray-50 bg-gradient-to-r from-yellow-300 to-green-500 hover:bg-gradient-to-br active:bg-gradient-to-l font-medium rounded-full text-md px-8 py-4 text-center mr-2 mb-2">
-                      <Icon/>
-                      <span>Masuk</span>
-                    </button>
-                  </div>
-                </div>
-                <div class="mx-auto">
-                  <Circle/>
-                </div>
-              </div>                            
-            </div>
+    <div className="h-screen w-auto">
+      <div className="flex flex-col w-3/4 h-screen mx-auto">
+        <div className="h-min w-full my-4">
+          <Topbar />
         </div>
+        <div className="flex w-full h-screen items-center justify-center">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col justify-center">
+              <article className="text-wrap">
+                <h1 className="text-4xl font-bold mb-4 text-green-600">
+                  Solusi Terbaik untuk Memantau Kandang Ayam Anda!
+                </h1>
+                <p className="text-bromo-gray-500">
+                  Broiler Monitoring, aplikasi yang dirancang untuk meningkatkan
+                  efektifitas pemantauan kandang ayam secara efektif dan efisen
+                  secara real-time.
+                </p>
+              </article>
+              <div className="mt-8">
+                <a
+                  href="/signin"
+                  className="inline-flex items-center text-bromo-neutral-50 font-semibold bg-gradient-to-r from-yellow-300 to-green-500 hover:bg-gradient-to-br active:bg-gradient-to-l font-medium rounded-full text-md px-8 py-4 text-center mr-2 mb-2"
+                >
+                  <Icon />
+                  <span>Masuk</span>
+                </a>
+              </div>
+            </div>
+            <div className="mx-auto">
+              <Circle />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
