71122d8eaca9a3fee74d11b116fb3c11fd7b3828
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-06-14 13:02:44 +0700

    fix: Mengubah tampilan dashboard untuk menyesuaikan

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -3,6 +3,10 @@ import DashboardLayout from "../../components/Layout/DashboardLayout";
 import DropdownKandang from "../../components/Dropdown/DropdownKandang";
 import FormKlasifikasi from "../../components/Forms/FormKlasifikasi";
 import RoundedContainer from "../../components/Layout/RoundedContainer";
+import DonutChartSuhu from '../../components/Charts/DonutChartSuhu';
+import DonutChartKelembaban from '../../components/Charts/DonutChartKelembaban';
+import DonutChartAmonia from '../../components/Charts/DonutChartAmonia';
+import FormForecasting from '../../components/Forms/FormForecasting';
 
 const Beranda = () => {
     const [selectedKandang, setSelectedKandang] = useState(null);
@@ -10,11 +14,22 @@ const Beranda = () => {
     return (
         <DashboardLayout>
             <DropdownKandang setSelectedKandang={setSelectedKandang} />
-            <RoundedContainer label={'Forecasting'}>
-                {/* <DonutChart /> */}
-            </RoundedContainer>
+            <div className="flex space-x-4">
+                <RoundedContainer label={'Ringkasan'} width={'w-3/4'}>
+                    <div className="flex justify-evenly">
+                        <DonutChartSuhu idKandang={selectedKandang ? selectedKandang.id : null} />
+                        <DonutChartKelembaban idKandang={selectedKandang ? selectedKandang.id : null} />
+                        <DonutChartAmonia idKandang={selectedKandang ? selectedKandang.id : null} />
+                    </div>
+                </RoundedContainer>
+                <RoundedContainer label={'Forecasting'} width={'w-1/4'}>
+                    <FormForecasting />
+                </RoundedContainer>
+            </div>
             <RoundedContainer label={'Klasifikasi'}>
-                <FormKlasifikasi idKandang={selectedKandang ? selectedKandang.id_kandang : null} />
+                {selectedKandang ? (
+                    <FormKlasifikasi idKandang={selectedKandang.id} />
+                ) : null}
             </RoundedContainer>
         </DashboardLayout>
     );
