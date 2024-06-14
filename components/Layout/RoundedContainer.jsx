14855cb38b3ccbdd1d4429d1f2a626e1675cbaf5
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-06-14 13:02:22 +0700

    fix: Tambah parameter untuk mengatur width dengan taiolwind css

--- components/Layout/RoundedContainer.jsx
+++ components/Layout/RoundedContainer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const RoundedContainer = ({ label, children }) => {
+const RoundedContainer = ({ label, children, width }) => {
   return (
-    <div className="rounded-lg border border-stroke bg-neutral-50 px-5 py-3 shadow-default sm:px-7.5 xl:pb-3">
+    <div className={`rounded-lg border border-stroke bg-neutral-50 px-5 py-3 shadow-default sm:px-7.5 xl:pb-3 shadow-md sha ${width}`}>
       {label && (
         <div className="border-b px-6.5 pb-1 mb-4">
           <h3 className="font-bold text-lg text-bromo-gray-900">
@@ -13,6 +14,16 @@ const RoundedContainer = ({ label, children }) => {
       {children}
     </div>
   );
+};
+
+RoundedContainer.propTypes = {
+  label: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  width: PropTypes.string, // Added PropTypes for the width
+};
+
+RoundedContainer.defaultProps = {
+  width: 'w-full', // Default width is full width
 };
 
 export default RoundedContainer;
