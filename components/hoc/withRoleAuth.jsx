77f466b32f9f0538140255037686e4bf3e795475
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-06-23 02:04:18 +0700

    feat: Menambahkan hoc withRoleAuth untuk pengamanan akses laman website agar ahany pengguna yang terdaftar yang dapat mengakses halaman

---
+++ components/hoc/withRoleAuth.jsx
@@ -0,0 +1,59 @@
+// components/hoc/withRoleAuth.jsx
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+const withRoleAuth = (WrappedComponent, allowedRoles) => {
+    return (props) => {
+        const [isAuthorized, setIsAuthorized] = useState(false);
+        const router = useRouter();
+
+        useEffect(() => {
+            const token = localStorage.getItem('accessToken');
+            if (!token) {
+                router.replace('/login');
+                return;
+            }
+
+            const fetchUserData = async () => {
+                try {
+                    const userId = localStorage.getItem('userId');
+                    const response = await fetch(`https://toko.technosv.my.id/api/user/${userId}`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`,
+                            'Content-Type': 'application/json',
+                        },
+                    });
+
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch user data');
+                    }
+
+                    const data = await response.json();
+                    console.log('User data:', data);
+
+                    const userRole = data.data.status; // Correctly extract the user role
+                    console.log('User role:', userRole);
+
+                    if (allowedRoles.includes(userRole)) {
+                        setIsAuthorized(true);
+                    } else {
+                        router.replace('/unauthorized');
+                    }
+                } catch (error) {
+                    console.error('Error fetching user data:', error);
+                    router.replace('/login');
+                }
+            };
+
+            fetchUserData();
+        }, []);
+
+        if (!isAuthorized) {
+            return null;
+        }
+
+        return <WrappedComponent {...props} />;
+    };
+};
+
+export default withRoleAuth;
