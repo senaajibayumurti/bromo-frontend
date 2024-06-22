7465717c51453b58efefdf066dbf14193d87bf67
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-06-23 02:17:25 +0700

    fix: Autorisasi halaman pekerja

--- pages/dashboard/pekerja/index.jsx
+++ pages/dashboard/pekerja/index.jsx
@@ -1,13 +1,19 @@
+// pages/dashboard/pekerja/Pekerja.jsx
+import React from 'react';
 import DashboardLayout from "../../../components/Layout/DashboardLayout";
 import RoundedContainer from "../../../components/Layout/RoundedContainer";
 import TablePekerja from "../../../components/Tables/TablePekerja";
+import withRoleAuth from '../../../components/hoc/withRoleAuth';
 
-export default function Pekerja(children) {
+const Pekerja = () => {
     return (
         <DashboardLayout>
-            <RoundedContainer>
-                <TablePekerja/>
+            <RoundedContainer label={'Data Pekerja'}>
+                <TablePekerja />
             </RoundedContainer>
         </DashboardLayout>
     );
-}
+};
+
+// Wrap the component with withRoleAuth and specify allowed roles
+export default withRoleAuth(Pekerja, ['owner']);
