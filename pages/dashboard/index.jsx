861b07456be5b0691f3421c106ab64e27f4383c9
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-06-23 02:18:56 +0700

    fix: Apply HOC dan decission bagi assigned atau unassigned anak kandang

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -7,36 +7,46 @@ import DonutChartSuhu from '../../components/Charts/DonutChartSuhu';
 import DonutChartKelembaban from '../../components/Charts/DonutChartKelembaban';
 import DonutChartAmonia from '../../components/Charts/DonutChartAmonia';
 import FormForecasting from '../../components/Forms/FormForecasting';
-import Breadcrumbs from '../../components/Navigation/BreadCrumb';
+import withRoleAuth from '../../components/hoc/withRoleAuth';
+import UnassignedWorker from '../../components/Layout/UnassignedWorker';
 
 const Beranda = () => {
     const [selectedKandang, setSelectedKandang] = useState(null);
+    const [kandangNotFound, setKandangNotFound] = useState(false);
 
     return (
         <DashboardLayout>
-            <DropdownKandang setSelectedKandang={setSelectedKandang} />
-            <div className="flex space-x-4">
-                <RoundedContainer label={'Ringkasan'} width={'w-full'}>
-                    <div className="flex justify-evenly">
-                        <DonutChartSuhu idKandang={selectedKandang ? selectedKandang.id : null} />
-                        <DonutChartKelembaban idKandang={selectedKandang ? selectedKandang.id : null} />
-                        <DonutChartAmonia idKandang={selectedKandang ? selectedKandang.id : null} />
-                        WARNING
+            {kandangNotFound ? (
+                <RoundedContainer>
+                    <UnassignedWorker/>
+                </RoundedContainer>
+            ) : (
+                <>
+                    <DropdownKandang setSelectedKandang={setSelectedKandang} setKandangNotFound={setKandangNotFound} />
+                    <div className="flex space-x-4">
+                        <RoundedContainer label={'Ringkasan'} width={'w-full'}>
+                            <div className="flex justify-evenly">
+                                <DonutChartSuhu idKandang={selectedKandang ? selectedKandang.id : null} />
+                                <DonutChartKelembaban idKandang={selectedKandang ? selectedKandang.id : null} />
+                                <DonutChartAmonia idKandang={selectedKandang ? selectedKandang.id : null} />
+                                WARNING
+                            </div>
+                        </RoundedContainer>
                     </div>
-                </RoundedContainer>
-            </div>
-            <div className="flex space-x-4">
-                <RoundedContainer label={'Klasifikasi'} width={'w-3/4'}>
-                    {selectedKandang ? (
-                        <FormKlasifikasi idKandang={selectedKandang.id} />
-                    ) : null}
-                </RoundedContainer>
-                <RoundedContainer label={'Forecasting'} width={'w-1/4'}>
-                    <FormForecasting />
-                </RoundedContainer>
-            </div>
+                    <div className="flex space-x-4">
+                        <RoundedContainer label={'Klasifikasi'} width={'w-3/4'}>
+                            {selectedKandang ? (
+                                <FormKlasifikasi idKandang={selectedKandang.id} />
+                            ) : null}
+                        </RoundedContainer>
+                        <RoundedContainer label={'Forecasting'} width={'w-1/4'}>
+                            <FormForecasting />
+                        </RoundedContainer>
+                    </div>
+                </>
+            )}
         </DashboardLayout>
     );
 };
 
-export default Beranda;
+export default withRoleAuth(Beranda, ['owner', 'anak kandang']);
