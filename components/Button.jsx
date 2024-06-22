02bd2139dc83e895a36f908d02130238b4e6bf03
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-06-23 02:02:51 +0700

    fix: Minor edit tampilan Button

--- components/Button.jsx
+++ components/Button.jsx
@@ -5,22 +5,22 @@ const Button = ({ label, onClick, type, disabled }) => {
     let buttonStyle = '';
     switch (type) {
         case 'success':
-            buttonStyle = 'bg-bromo-success-500 hover:bg-bromo-success-600 active:bg-bromo-success-700 text-bromo-neutral-50';
+            buttonStyle = 'bg-bromo-success-500 hover:bg-bromo-success-600 active:bg-bromo-success-700';
             break;
         case 'info':
-            buttonStyle = 'bg-bromo-info-500 hover:bg-bromo-info-600 active:bg-bromo-info-700 text-bromo-neutral-900';
+            buttonStyle = 'bg-bromo-info-500 hover:bg-bromo-info-600 active:bg-bromo-info-700';
             break;
         case 'error':
-            buttonStyle = 'bg-bromo-error-500 hover:bg-bromo-error-600 active:bg-bromo-error-700 text-bromo-neutral-50';
+            buttonStyle = 'bg-bromo-error-500 hover:bg-bromo-error-600 active:bg-bromo-error-700';
             break;
         default:
-            buttonStyle = 'bg-bromo-green-500 hover:bg-bromo-green-600 active:bg-bromo-green-500 text-bromo-neutral-50';
+            buttonStyle = 'bg-bromo-green-500 hover:bg-bromo-green-600 active:bg-bromo-green-500';
     }
 
     return (
         <button
             type="button"
-            className={`px-3 py-1 rounded-lg text-sm ${buttonStyle} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+            className={`px-5 py-1.5 rounded-lg text-sm text-bromo-neutral-50 font-bold ${buttonStyle} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
             onClick={onClick}
             disabled={disabled}
         >
