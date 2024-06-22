2ee8380baaee20d40043fbeff147167af5e73c03
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2024-06-23 02:09:46 +0700

    feat: Komponen baru untuk memberikan feedback bahwa pengguna belum disuruh menjaga kandang

---
+++ components/Layout/UnassignedWorker.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UnassignedWorker = () => {
+    return (
+        <div className="text-red-500 text-lg font-bold">
+            Anda belum disuruh jaga kandang
+        </div>
+    );
+};
+
+export default UnassignedWorker;
